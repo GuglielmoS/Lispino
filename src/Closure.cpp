eb999668ed9cd266d99fa0a26750985712eb3aff
GuglielmoS <guglielmo@fachini.net>
2014-02-12 23:29:28 +0100

    little changes

--- src/Closure.cpp
+++ src/Closure.cpp
@@ -4,33 +4,13 @@
 using namespace Lispino;
 
 Object* Closure::apply(std::vector<Object*>& actualArgs) {
-    std::vector<std::string> formalArgs = lambda->getArguments();
+    std::vector<std::string>& formalArgs = lambda->getArguments();
+
     if (formalArgs.size() != actualArgs.size())
         throw std::runtime_error("Invalid function call, wrong number of arguments");
 
-   /* 
-    std::cout << std::endl;
-    std::cout << "CLOSURE APPLICATION:" << std::endl;
-    std::cout << "  ARGUMENTS: ";
-    for (unsigned int i = 0; i < formalArgs.size(); i++)
-        std::cout << formalArgs[i] << " = " << actualArgs[i]->toString() << ", ";
-    std::cout << std::endl;
-    */
-
     for (unsigned int i = 0; i < formalArgs.size(); i++)
         env->put(VM::getAllocator().createSymbol(formalArgs[i]), actualArgs[i]);
-
-    /*
-    if (env->getParentEnv() != nullptr)
-        std::cout << "  ENCLOSING ENV: " << env->getParentEnv()->summary() << std::endl;
-    std::cout << "  ENV:           " << env->summary() << std::endl;
-    std::cout << "  ENV ADDRESS: " << env.get() << std::endl;
-
-    std::cout << std::endl;
-    std::cout << "LAMBDA BODY: " << lambda->getBody() << std::endl;
-    std::cout << "IS LAMBDA BODY AN IF? " << lambda->getBody()->isIfExpr() << std::endl;
-    std::cout << "I hope so!\n";
-    */
 
     return lambda->getBody()->eval(*env);
 }
--- test/interpreter_tests.cpp
+++ test/interpreter_tests.cpp
@@ -119,6 +119,74 @@ TEST(InterpreterTests, BuiltinSub) {
     ASSERT_EQ(0.5, static_cast<FloatNumber*>(expr)->getValue());
 }
 
+TEST(InterpreterTests, BuiltinMul) {
+    std::stringstream stream("(* 0 1) (* 1 (* 2 (* 3 4))) (* 1 0.5)");
+    Parser parser(&stream);
+
+    // parse the stream and check the expressions
+    Object *expr(parser.parseExpr()->eval());
+    ASSERT_TRUE(expr->isIntNumber());
+    ASSERT_EQ(0, static_cast<IntNumber*>(expr)->getValue());
+
+    expr = parser.parseExpr()->eval();
+    ASSERT_TRUE(expr->isIntNumber());
+    ASSERT_EQ(24, static_cast<IntNumber*>(expr)->getValue());
+
+    expr = parser.parseExpr()->eval();
+    ASSERT_TRUE(expr->isFloatNumber());
+    ASSERT_FLOAT_EQ(0.5, static_cast<FloatNumber*>(expr)->getValue());
+}
+
+TEST(InterpreterTests, BuiltinDiv) {
+    std::stringstream stream("(/ 0 1) (/ 1 2) (/ 4 2)");
+    Parser parser(&stream);
+
+    // parse the stream and check the expressions
+    Object *expr(parser.parseExpr()->eval());
+    ASSERT_TRUE(expr->isFloatNumber());
+    ASSERT_FLOAT_EQ(0.0f, static_cast<FloatNumber*>(expr)->getValue());
+
+    expr = parser.parseExpr()->eval();
+    ASSERT_TRUE(expr->isFloatNumber());
+    ASSERT_FLOAT_EQ(0.5f, static_cast<FloatNumber*>(expr)->getValue());
+
+    expr = parser.parseExpr()->eval();
+    ASSERT_TRUE(expr->isFloatNumber());
+    ASSERT_FLOAT_EQ(2.0f, static_cast<FloatNumber*>(expr)->getValue());
+}
+
+TEST(InterpreterTests, BuiltinRemainder) {
+    std::stringstream stream("(remainder 1 2) (remainder 5 2) (remainder 4 2)");
+    Parser parser(&stream);
+
+    // parse the stream and check the expressions
+    Object *expr(parser.parseExpr()->eval());
+    ASSERT_TRUE(expr->isIntNumber());
+    ASSERT_EQ(1, static_cast<IntNumber*>(expr)->getValue());
+
+    expr = parser.parseExpr()->eval();
+    ASSERT_TRUE(expr->isIntNumber());
+    ASSERT_EQ(1, static_cast<IntNumber*>(expr)->getValue());
+
+    expr = parser.parseExpr()->eval();
+    ASSERT_TRUE(expr->isIntNumber());
+    ASSERT_EQ(0, static_cast<IntNumber*>(expr)->getValue());
+}
+
+TEST(InterpreterTests, BuiltinDisplay) {
+    std::stringstream stream("(display 1 2 3) (display \"(+ 1 1) = \" (+ 1 1))");
+    Parser parser(&stream);
+
+    // parse the stream and check the expressions
+    Object *expr(parser.parseExpr()->eval());
+    ASSERT_TRUE(expr->isString());
+    ASSERT_EQ("123", static_cast<String*>(expr)->getValue());
+
+    expr = parser.parseExpr()->eval();
+    ASSERT_TRUE(expr->isString());
+    ASSERT_EQ("(+ 1 1) = 2", static_cast<String*>(expr)->getValue());
+}
+
 TEST(InterpreterTests, Environment) {
     std::stringstream stream("(define x 1) (define y 2) (define z (cons x y))");
     Parser parser(&stream);
@@ -202,6 +270,25 @@ TEST(InterpreterTests, BuiltinEqual) {
     expr = parser.parseExpr()->eval();
     ASSERT_TRUE(expr->isBoolean());
     ASSERT_TRUE(static_cast<Boolean*>(expr)->isFalse());
+}
+
+TEST(InterpreterTests, BuiltinGreaterThan) {
+    std::stringstream stream("(> 0 0) (> 0 1) (> 1 0) (> 2.1 2)");
+    Parser parser(&stream);
+
+    // parse the stream and check the expressions
+    Object *expr(parser.parseExpr()->eval());
+    ASSERT_TRUE(expr->isBoolean());
+    ASSERT_TRUE(static_cast<Boolean*>(expr)->isFalse());
+    expr = parser.parseExpr()->eval();
+    ASSERT_TRUE(expr->isBoolean());
+    ASSERT_TRUE(static_cast<Boolean*>(expr)->isFalse());
+    expr = parser.parseExpr()->eval();
+    ASSERT_TRUE(expr->isBoolean());
+    ASSERT_TRUE(static_cast<Boolean*>(expr)->isTrue());
+    expr = parser.parseExpr()->eval();
+    ASSERT_TRUE(expr->isBoolean());
+    ASSERT_TRUE(static_cast<Boolean*>(expr)->isTrue());
 }
 
 TEST(InterpreterTests, IfExpr) {
