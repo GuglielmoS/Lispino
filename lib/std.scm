81b338c7124f0254f1040d9193bad17b9dac85a6
GuglielmoS <guglielmo@fachini.net>
2014-09-09 19:13:59 +0200

    added some other functions to the standard library

--- lib/std.scm
+++ lib/std.scm
@@ -27,19 +27,52 @@
       (cons (car lst) (filter pred (cdr lst)))
       (filter pred (cdr lst)))))
 
-(define (any lst)
-  (fold or false lst))
+(define (takeWhile f lst)
+  (if (or (null? lst) (false? (f (car lst))))
+    nil
+    (cons (car lst) (takeWhile f (cdr lst)))))
 
-(define (all lst)
-  (fold and true lst))
+(define (dropWhile f lst)
+  (if (or (null? lst) (false? (f (car lst))))
+    lst
+    (dropWhile f (cdr lst))))
 
 (define (fold f init lst)
   (if (null? lst)
     init
     (f (car lst) (fold f init (cdr lst)))))
 
+(define (zipWith f lst1 lst2)
+  (if (or (null? lst1) (null? lst2))
+    nil
+    (cons (f (car lst1) (car lst2))
+          (zipWith f (cdr lst1) (cdr lst2)))))
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;:::::::::::::::
 ;; List                                                                      ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(define (any lst)
+  (fold or false lst))
+
+(define (all lst)
+  (fold and true lst))
+
+(define (take n lst)
+  (if (or (zero? n) (null? lst))
+    nil
+    (cons (car lst) (take (- n 1) (cdr lst)))))
+
+(define (drop n lst)
+  (if (or (zero? n) (null? lst))
+    lst
+    (drop (- n 1) (cdr lst))))
+
+(define (zip lst1 lst2)
+  (zipWith cons lst1 lst2))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;:::::::::::::::
+;; Math                                                                      ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (define (range start end)
@@ -53,10 +86,27 @@
 (define (product lst)
   (fold * 1 lst))
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;:::::::::::::::
-;; Math                                                                      ;;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(define (zero? n)
+  (= n 0))
+
+(define (positive? n)
+  (> n 0))
 
 (define (factorial n)
   (product (range 1 (+ n 1))))
+
+(define (even n)
+  (if (zero? n)
+    true
+    (odd (- n 1))))
+
+(define (odd n)
+  (if (zero? n)
+    false
+    (even (- n 1))))
+
+(define (fibonacci n)
+  (if (< n 2)
+    1
+    (+ (fibonacci (- n 1)) (fibonacci (- n 2)))))
 
