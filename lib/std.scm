eb1cf085a2808a7070b9d5cb7919d8ec31d8a313
GuglielmoS <guglielmo@fachini.net>
2014-09-09 16:05:10 +0200

    fixed the indentation level in the standard library

--- lib/std.scm
+++ lib/std.scm
@@ -3,60 +3,60 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (define (not p)
-  (if p false true))
+	(if p false true))
 
 (define (and p1 p2)
-  (if p1 p2 false))
+	(if p1 p2 false))
 
 (define (or p1 p2)
-  (if p1 true p2))
+	(if p1 true p2))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;:::::::::::::::
 ;; Higher order functions                                                    ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (define (map f lst)
-  (if (null? lst)
-    nil
-    (cons (f (car lst)) (map f (cdr lst)))))
+	(if (null? lst)
+		nil
+		(cons (f (car lst)) (map f (cdr lst)))))
 
 (define (filter pred lst)
-  (if (null? lst)
-    nil
-    (if (pred (car lst))
-      (cons (car lst) (filter pred (cdr lst)))
-      (filter pred (cdr lst)))))
+	(if (null? lst)
+		nil
+		(if (pred (car lst))
+			(cons (car lst) (filter pred (cdr lst)))
+			(filter pred (cdr lst)))))
 
 (define (any lst)
-  (fold or false lst))
+	(fold or false lst))
 
 (define (all lst)
-  (fold and true lst))
+	(fold and true lst))
 
 (define (fold f init lst)
-  (if (null? lst)
-	init
-	(f (car lst) (fold f init (cdr lst)))))
+	(if (null? lst)
+		init
+		(f (car lst) (fold f init (cdr lst)))))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;:::::::::::::::
 ;; List                                                                      ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (define (range start end)
-  (if (= start end)
-    nil
-    (cons start (range (+ start 1) end))))
+	(if (= start end)
+		nil
+		(cons start (range (+ start 1) end))))
 
 (define (sum lst)
-  (fold + 0 lst))
+	(fold + 0 lst))
 
 (define (product lst)
-  (fold * 1 lst))
+	(fold * 1 lst))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;:::::::::::::::
 ;; Math                                                                      ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (define (factorial n)
-  (product (range 1 (+ n 1))))
+	(product (range 1 (+ n 1))))
 
