c060fa7bd8ad2f6da99d7563a17bf1709695e6a2
GuglielmoS <guglielmo@fachini.net>
2014-09-09 16:09:06 +0200

    fixed the indentation level in the standard library for the third time!

--- lib/std.scm
+++ lib/std.scm
@@ -3,7 +3,7 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (define (not p)
-  (if p false true))
+	(if p false true))
 	
 (define (and p1 p2)
 	(if p1 p2 false))
