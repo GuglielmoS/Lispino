605284dec142119ea2faf5179436ecb5f71b5756
GuglielmoS <guglielmo@fachini.net>
2014-09-23 13:59:06 +0200

    added an example with the Y combinator and the implementations of factorial and fibonacci in terms of it

---
+++ examples/ycombinator.scm
@@ -0,0 +1,30 @@
+;; Taken from http://rosettacode.org/wiki/Y_combinator#Scheme
+
+(define (Y f)
+  ((lambda (x)
+     (x x))
+   (lambda (g)
+     (f (lambda (args)
+          ((g g) args))))))
+
+(define fac
+  (Y
+    (lambda (f)
+      (lambda (x)
+        (if (< x 2)
+          1
+          (* x (f (- x 1))))))))
+
+(define fib
+  (Y
+    (lambda (f)
+      (lambda (x)
+        (if (< x 2)
+          x
+          (+ (f (- x 1)) (f (- x 2))))))))
+
+(display "Factorial(6) = " (fac 6))
+(newline)
+
+(display "Fibonacci(6) = " (fib 6))
+(newline)
