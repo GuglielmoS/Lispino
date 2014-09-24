de89fa64e5ce4e9c5a90f3f9ee2aa9f4039348f6
GuglielmoS <guglielmo@fachini.net>
2014-09-24 15:37:39 +0200

    added the SICP example on symbolic differetiation

---
+++ examples/symbolic_differentiation.scm
@@ -0,0 +1,50 @@
+(define (deriv expr var)
+  (cond ((number? expr) 0)
+        ((variable? expr)
+         (if (same-variable? expr var) 1 0))
+        ((sum? expr)
+         (make-sum (deriv (addend expr) var)
+                   (deriv (augend expr) var)))
+        ((product? expr)
+         (make-sum
+           (make-product (multiplier expr)
+                         (deriv (multiplicand expr) var))
+           (make-product (deriv (multiplier expr) var)
+                         (multiplicand expr))))
+        (else
+          "unknown-expression-type")))
+
+(define (variable? x) (symbol? x))
+
+(define (same-variable? v1 v2)
+  (and (variable? v1) (variable? v2) (= v1 v2)))
+
+(define (make-sum a1 a2)
+  (cond ((=number? a1 0) a2)
+        ((=number? a2 0) a1)
+        ((and (number? a1) (number? a2)) (+ a1 a2))
+        (else (list '+ a1 a2))))
+
+(define (=number? expr num)
+  (if (number? expr) (= expr num) #f))
+
+(define (make-product m1 m2)
+  (cond ((or (=number? m1 0) (=number? m2 0)) 0)
+        ((=number? m1 1) m2)
+        ((=number? m2 1) m1)
+        ((and (number? m1) (number? m2)) (* m1 m2))
+        (else (list '* m1 m2))))
+
+(define (sum? x)
+  (and (list? x) (= (car x) '+)))
+
+(define (addend s) (car (cdr s)))
+
+(define (augend s) (car (cdr (cdr s))))
+
+(define (product? x)
+  (and (list? x) (= (car x) '*)))
+
+(define (multiplier p) (car (cdr p)))
+
+(define (multiplicand p) (car (cdr (cdr p))))
